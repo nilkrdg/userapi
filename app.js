19f0c90c0481bed127ca4f90b32283668c6951c6
Nil Karadag <nil.karadag@vestel.com.tr>
2018-06-28 18:36:08 +0300

    routes have been seperated from main server script.

--- app.js
+++ app.js
@@ -2,136 +2,33 @@ const express = require('express');
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
- flash = require('connect-flash');
-
+    flash = require('connect-flash'),
+    routes = require('./lib/routes'),
+        apiRoutes = require('./lib/apiRoutes');
 
 const db = mongoose.connect('mongodb://localhost/userAPI');
-const User = require('./models/userModel');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//to show messages after redirect
+//To show messages after redirect
 app.use(flash());
 
 app.use(session({ secret: 'example' }));
 
-//set view engine pug and its source folder
+//Set view engine pug and its source folder
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-let userRouter = express.Router();
 
+//Connect routes starting with api
+app.use('/api', apiRoutes);
 
-// middleware to use for all requests
-userRouter.use((req, res, next) => {
-    //logging
-    console.log('method '+ req.method);
-    console.log('originalUrl '+req.originalUrl);
-    console.log('params '+req.params);
-    console.log('query '+req.query);
-    next(); // go to the next routes
-});
-
-userRouter.route('/users')
-    .post((req, res) => {
-        var user = new User(req.body);
-        console.log(user);
-        user.save((err, user) => {
-            if(err)
-            {
-                console.log(err);
-                res.status(400).send(err.message);
-            }
-            else{
-                res.status(201).send(user);
-            }
-        });
-    })
-    .get((req, res) => {
-
-        User.find((err, users) => {
-            if(err)
-            {
-                res.status(500).send(err);
-            }else{
-                res.json(users);
-            }
-        });
-    });
-const SEND_UPDATED_DOC = true;
-userRouter.route('/users/:id')
-    .get((req, res) => {
-        res.locals.session = req.session;
-        res.render('userDetails');
-    })
-    .delete((req, res) => {
-        User.findByIdAndRemove(req.params.id, (err, user) => {
-            if (err){
-                return res.status(500).send(err);
-            }
-            const response = {
-                message: "User successfully deleted",
-                id: user.id
-            };
-            return res.status(200).send(response);
-        });
-    })
-    .put((req, res) => {
-        User.findByIdAndUpdate(req.params.id, req.body,
-            {new: SEND_UPDATED_DOC},
-            (err, user) => {
-                if (err){
-                    return res.status(500).send(err);
-                }
-                return res.status(200).send(user);
-            }
-        )
-    });
-
-
-
-app.use('/api', userRouter);
-
-
-app.get('/',(req, res) =>
-{
-    res.render('index', { flash: req.flash() });
-});
-
-app.post('/',(req, res) =>
-{
-    User.findOne({"username":req.body.username},(err, user) => {
-        if(err || !user)
-        {
-            req.flash('error', 'User not found');
-            res.redirect('/');
-
-        }else{
-            user.comparePassword(req.body.password, (err, isMatched) => {
-                if (err){
-                    req.flash('error', 'Username or password is incorrect');
-                    res.redirect('/');
-                }
-                if(isMatched) {
-                    req.session.authenticated = true;
-                    req.session.user = user;
-                    res.locals.session = req.session;
-                    req.flash('success', 'Authentication successful');
-                    //res.redirect('/api/users/'+user.id);
-
-                    res.render('welcome', { flash: req.flash() });
-                }
-                else{
-                    req.flash('error', 'Username or password is incorrect');
-                    res.redirect('/');
-                }
-            });
-        }
-    });
-});
+//Connect all other routes
+app.use('/', routes);
 
 app.listen(port, () => console.log("gulp is running : listening on port "+port));
 
