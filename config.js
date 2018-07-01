2fd76da3c1530c247db60a3f6e0bb577f87e144b
Nil Karadag <nil.karadag@vestel.com.tr>
2018-07-01 16:10:47 +0300

    Token time to live value has been set to 24 hours.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'port':8080,
     'host':'localhost',
     'secret': 'mysecret',
-    'tokenTimeToLive': 60, //expires in 24 hours
+    'tokenTimeToLive': 86400, //expires in 24 hours
     'salt': 10 //static salt value for hashing password
     // Single salt value is used for simplicity, security might be enhanced by using different salt for each user
 };
